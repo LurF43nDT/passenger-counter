2aaf3af18de796d35c55d271a377c59cb1dfffbe
LurF43nDT <simon.digitaletalenter@gmail.com>
2024-04-16 16:54:04 +0200

    fixing save function

--- index.js
+++ index.js
@@ -2,18 +2,19 @@ let saveEl = document.getElementById("save-el")
 let countEL = document.getElementById("count-el")
 let count = 0
 
-console.log(saveEl)
+
 
 function increment() {
     console.log("clicked")
     count += 1
-    countEL.innerText = count
+    countEL.textContent = count
 }
 
 function save(){
     let countStr = count + " - "
-    saveEl.innerText += countStr
-    console.log(count)
+    saveEl.textContent += countStr
+    countEL.textContent = 0
+
 }
 
 save()
